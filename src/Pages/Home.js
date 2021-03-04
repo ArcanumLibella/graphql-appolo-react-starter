4db9e4311aa48cf41a20569f49ec25b27d31ee8a
ArcanumLibella <fiona.roux@hetic.net>
2021-03-04 10:33:51 +0100

    feat: link query to Home component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
+import { GET_WEATHER_QUERY } from '../graphql/Queries';
 
 function Home() {
+  const [citySearched, setCitySearched] = useState('');
+
+  const [getWeather, {loading, data, error}] = useLazyQuery(GET_WEATHER_QUERY, {
+    variables: {name: citySearched},
+  });
+
+  if (error) return <h2>Il y a une erreur</h2>
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <div className="home">
       <h1>Mon application Météo</h1>
-      <input type="text" placeholder="Nom de la ville..." />
-      <button>Rechercher</button>
+      <input 
+        type="text" 
+        placeholder="Nom de la ville..."
+        onChange={(event) => setCitySearched(event.target.value)} 
+      />
+      <button onClick={() => getWeather()}>Rechercher</button>
     </div>
   )
 }
--- src/graphql/Queries.js
+++ src/graphql/Queries.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const GET_WEATHER_QUERY = gql`
-  query getCityByName(name: String!) {
+  query getCityByName($name: String!) {
     getCityByName(name: $name) {
       name
       country
